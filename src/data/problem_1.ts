5564ef7d87914d81c654c1e65afb993ad5452444
bennyk0406 <kbenny0406@gmail.com>
2023-11-01 23:22:30 +0900

    Modify problems

--- src/data/problem_1.ts
+++ src/data/problem_1.ts
@@ -3,17 +3,14 @@ const F = false
 
 export default {
     width: 7,
-    height: 7,
+    height: 4,
     wall: [
         [T, T, T, T, T, T, T],
         [T, F, F, F, F, F, T],
         [T, F, F, F, F, F, T],
-        [T, F, F, F, F, F, T],
-        [T, F, F, F, F, F, T],
-        [T, F, F, F, F, F, T],
         [T, T, T, T, T, T, T]
     ],
-    goal: [4, 4],
-    rock: [2, 2],
-    position: [3, 4]
+    goal: [1, 4],
+    rock: [1, 2],
+    position: [1, 1]
 }
