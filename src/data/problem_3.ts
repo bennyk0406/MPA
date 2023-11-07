0c394da338cc5d0a6917898531707614180ae5ab
sooahn07 <23-022@ksa.hs.kr>
2023-11-08 08:35:29 +0900

    make hole in problem 3

--- src/data/problem_3.ts
+++ src/data/problem_3.ts
@@ -22,7 +22,7 @@ export default {
         [T, F, F, F, T, F, F, F, F, F, F, F, T, F, T, F, F, T, F, T],
         [T, F, F, F, T, F, F, F, F, F, F, F, F, T, F, T, F, T, F, T],
         [T, F, F, F, F, F, F, T, F, F, F, F, F, F, F, F, T, T, F, T],
-        [T, F, F, F, F, F, F, F, T, T, T, T, F, F, T, F, F, T, F, T],
+        [T, F, F, F, F, F, F, F, T, F, T, T, F, F, T, F, F, T, F, T],
         [T, F, F, F, T, F, F, F, F, F, T, T, F, T, T, T, F, F, F, T],
         [T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T]
     ],
