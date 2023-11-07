7bd6d844ac843effaa9335e0d80d8bc851971d51
Modular Inverse <125933200+modularinv@users.noreply.github.com>
2023-11-07 15:51:49 +0900

    Update problem_1.ts

--- src/data/problem_1.ts
+++ src/data/problem_1.ts
@@ -2,15 +2,20 @@ const T = true
 const F = false
 
 export default {
-    width: 7,
-    height: 4,
+    width: 9,
+    height: 9,
     wall: [
-        [T, T, T, T, T, T, T],
-        [T, F, F, F, F, F, T],
-        [T, F, F, F, F, F, T],
-        [T, T, T, T, T, T, T]
+        [T, T, T, T, T, T, T, T, T],
+        [T, F, F, T, F, F, F, F, T],
+        [T, F, F, F, F, T, T, F, T],
+        [T, F, F, F, F, F, T, F, T],
+        [T, F, T, F, F, F, T, F, T],
+        [T, F, T, T, F, F, F, F, T],
+        [T, F, T, T, F, F, F, F, T],
+        [T, F, F, F, F, F, F, F, T],
+        [T, T, T, T, T, T, T, T, T]
     ],
-    goal: [1, 4],
-    rock: [1, 2],
-    position: [1, 1]
+    goal: [6, 6],
+    rock: [2, 2],
+    position: [1, 2]
 }
