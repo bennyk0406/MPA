0c50e13d74ea39d34c222cd7b4741e2ff421dc6f
sooahn07 <23-022@ksa.hs.kr>
2023-11-08 08:25:02 +0900

    change probelm 3 to 2 and add 3

--- src/data/problem_2.ts
+++ src/data/problem_2.ts
@@ -2,23 +2,18 @@ const T = true
 const F = false
 
 export default {
-    width: 11,
-    height: 12,
+    width: 7,
+    height: 7,
     wall: [
-        [T, T, T, T, T, T, T, T, T, T, T],
-        [T, F, F, F, F, F, F, F, F, F, T],
-        [T, F, F, F, F, F, F, F, F, F, T],
-        [T, F, F, F, F, F, F, F, F, F, T],
-        [T, F, F, F, T, T, T, T, T, T, T],
-        [T, F, F, F, F, F, F, F, F, F, T],
-        [T, F, F, F, F, F, F, F, F, F, T],
-        [T, F, F, F, F, F, F, F, F, F, T],
-        [T, T, T, T, T, T, T, F, F, F, T],
-        [T, F, F, F, F, F, F, F, F, F, T],
-        [T, F, F, F, F, F, F, F, F, F, T],
-        [T, T, T, T, T, T, T, T, T, T, T],
+        [T, T, T, T, T, T, T],
+        [T, F, F, F, F, F, T],
+        [T, F, F, F, F, F, T],
+        [T, F, F, F, F, F, T],
+        [T, F, F, F, F, F, T],
+        [T, F, F, F, F, F, T],
+        [T, T, T, T, T, T, T]
     ],
-    goal: [9, 1],
-    rock: [2, 7],
-    position: [6, 5]
+    goal: [4, 4],
+    rock: [2, 2],
+    position: [3, 4]
 }
--- src/data/problem_3.ts
+++ src/data/problem_3.ts
@@ -2,18 +2,31 @@ const T = true
 const F = false
 
 export default {
-    width: 7,
-    height: 7,
+    width: 20,
+    height: 20,
     wall: [
-        [T, T, T, T, T, T, T],
-        [T, F, F, F, F, F, T],
-        [T, F, F, F, F, F, T],
-        [T, F, F, F, F, F, T],
-        [T, F, F, F, F, F, T],
-        [T, F, F, F, F, F, T],
-        [T, T, T, T, T, T, T]
+        [T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T],
+        [T, T, F, F, F, F, T, F, F, T, F, F, F, F, F, F, F, F, T, T],
+        [T, F, F, F, F, F, F, F, F, T, F, F, F, F, F, F, T, F, F, T],
+        [T, F, F, F, F, F, F, F, F, T, F, F, F, F, F, F, F, T, F, T],
+        [T, F, T, T, T, T, F, F, F, T, T, F, F, T, F, F, F, F, F, T],
+        [T, F, F, F, F, F, T, F, F, T, F, F, F, F, T, F, F, F, F, T],
+        [T, F, F, F, F, F, F, F, F, F, F, F, F, F, T, F, F, F, F, T],
+        [T, F, F, T, F, F, T, F, F, F, F, F, F, F, T, F, F, F, F, T],
+        [T, T, F, F, F, F, T, F, F, F, T, T, F, F, F, T, F, F, F, T],
+        [T, F, F, F, F, F, F, T, T, T, F, F, T, T, T, F, F, F, T, T],
+        [T, F, F, F, T, F, F, F, F, F, F, F, F, F, F, F, F, F, F, T],
+        [T, T, F, F, T, T, T, F, F, F, F, T, F, F, F, F, F, T, F, T],
+        [T, F, F, F, F, F, F, T, F, T, F, F, F, F, F, F, F, T, F, T],
+        [T, F, F, F, F, T, F, T, F, F, F, F, F, F, F, T, F, T, F, T],
+        [T, F, F, F, T, F, F, F, F, F, F, F, T, F, T, F, F, T, F, T],
+        [T, F, F, F, T, F, F, F, F, F, F, F, F, T, F, T, F, T, F, T],
+        [T, F, F, F, F, F, F, T, F, F, F, F, F, F, F, F, T, T, F, T],
+        [T, F, F, F, F, F, F, F, T, T, T, T, F, F, T, F, F, T, F, T],
+        [T, F, F, F, T, F, F, F, F, F, T, T, F, T, T, T, F, F, F, T],
+        [T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T]
     ],
-    goal: [4, 4],
+    goal: [6, 1],
     rock: [2, 2],
-    position: [3, 4]
+    position: [18, 18]
 }
