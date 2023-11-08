f1dd5b4ab4abc80695d93549402d52dbcf16e1a9
bennyk0406 <kbenny0406@gmail.com>
2023-11-08 09:39:33 +0900

    Modify UI

---
+++ src/components/Modal.tsx
@@ -0,0 +1,26 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react"
+
+interface ModalProps {
+    children?: React.ReactNode
+}
+
+const Modal: React.FC<ModalProps> = (props) => {
+    return (
+        <div css={css`
+            position: fixed;
+            left: 0;
+            right: 0;
+            width: 100%;
+            height: 100%;
+            background-color: #00000088;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        `}>
+            {props.children}
+        </div>
+    )
+}
+
+export default Modal
