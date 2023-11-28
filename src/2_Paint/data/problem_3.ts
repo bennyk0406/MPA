c320884401d9cfe1548ea08b3530782a6f6c504e
sooahn07 <23-022@ksa.hs.kr>
2023-11-28 16:15:04 +0900

    add _ to unused colors

--- src/2_Paint/data/problem_3.ts
+++ src/2_Paint/data/problem_3.ts
@@ -1,6 +1,6 @@
 const colors = ["#FF0000", "#FFC000", "#FFFF00", "#92D050", "#00B0F0", "#9943D9"]
-const [R, _O, Y, G, B, P] = colors // Please insert underbar(_) in front of the variables which are not used.
-const _ = ""
+const [R, O, Y, G, B, P] = colors // Please insert underbar(_) in front of the variables which are not used.
+//const _ = ""
 
 const map = [
     [P, P, P, P, P, P, P, B],
--- src/2_Paint/data/problem_4.ts
+++ src/2_Paint/data/problem_4.ts
@@ -1,17 +1,17 @@
 const colors = ["#FF0000", "#FFC000", "#FFFF00", "#92D050", "#00B0F0", "#9943D9"]
-const [R, _O, Y, G, B, P] = colors // Please insert underbar(_) in front of the variables which are not used.
+const [R, O, Y, G, B, P] = colors // Please insert underbar(_) in front of the variables which are not used.
 const _ = ""
 
 const map = [
-    [_O, _O, R, _O, _O, _O, _O, _O, _O, _O],
-    [P, _, _, _, R, Y, _O, _, Y, _],
+    [O, O, R, O, O, O, O, O, O, O],
+    [P, _, _, _, R, Y, O, _, Y, _],
     [P, P, R, P, P, P, P, P, Y, P],
-    [_, _, R, _, R, _, _O, _, Y, _],
+    [_, _, R, _, R, _, O, _, Y, _],
     [B, B, R, B, B, B, _, B, B, B],
     [_, _, R, G, G, G, G, _, Y, G],
-    [_, _, R, _, R, Y, _O, _, Y, _],
-    [P, P, R, _, B, Y, _O, _, Y, _],
-    [_, _, R, _, R, Y, _O, _, Y, _],
+    [_, _, R, _, R, Y, O, _, Y, _],
+    [P, P, R, _, B, Y, O, _, Y, _],
+    [_, _, R, _, R, Y, O, _, Y, _],
     [G, G, R, G, G, G, G, G, G, G]
 ]
 
