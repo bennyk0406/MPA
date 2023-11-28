0586e11fa76739f17facc868ac51531fb2705597
sooahn07 <23-022@ksa.hs.kr>
2023-11-28 15:50:29 +0900

    add problem 3,4,5

---
+++ src/2_Paint/data/problem_3.ts
@@ -0,0 +1,16 @@
+const colors = ["#FF0000", "#FFC000", "#FFFF00", "#92D050", "#00B0F0", "#9943D9"]
+const [R, _O, Y, G, B, P] = colors // Please insert underbar(_) in front of the variables which are not used.
+const _ = ""
+
+const map = [
+    [P, P, P, P, P, P, P, B],
+    [B, R, B, P, B, P, R, B],
+    [B, R, B, P, B, P, R, B],
+    [B, R, R, P, B, P, R, B],
+    [P, P, P, P, P, P, R, B],
+    [B, R, R, P, B, P, R, B],
+    [P, P, P, P, P, P, R, B],
+    [B, R, P, P, B, P, R, B]
+]
+
+export { colors, map }
