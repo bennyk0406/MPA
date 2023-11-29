e90953602cda46a3c3d594b23951f62ba0d3b7aa
sooahn07 <23-022@ksa.hs.kr>
2023-11-29 10:16:14 +0900

    change problem 6

--- src/2_Paint/data/problem_6.ts
+++ src/2_Paint/data/problem_6.ts
@@ -1,15 +1,12 @@
 const colors = ["#FF0000", "#FFC000", "#FFFF00", "#92D050", "#00B0F0", "#9943D9"]
-const [R, _O, Y, G, B, P] = colors // Please insert underbar(_) in front of the variables which are not used.
+const [R, O, _Y, G, _B, _P] = colors // Please insert underbar(_) in front of the variables which are not used.
 const _ = ""
 
 const map = [
-    [R, _, _, G, Y, _],
-    [P, P, P, G, Y, P],
-    [R, _, _, G, Y, _],
-    [B, B, B, B, B, B],
-    [R, _, _, G, Y, _],
-    [R, _, _, G, Y, _],
-    [G, G, G, G, G, G]
+    [R, _, G, _],
+    [R, _, G, _],
+    [O, O, _, O],
+    [R, _, G, _]
 ]
 
 export { colors, map }
