9aac8fe1803713b1a3a1594320c05059fd87517b
bennyk0406 <kbenny0406@gmail.com>
2023-12-04 22:13:21 +0900

    2: Final

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<Lobby />} />
                     <Route path="/ramen" element={<Home title="#1. 라면 밀기" probNum={5} />} />
-                    <Route path="/paint" element={<Home title="#2. 강의실 도색하기" probNum={6} />} />
+                    <Route path="/paint" element={<Home title="#2. 강의실 바닥 꾸미기" probNum={5} />} />
                     <Route path="/ramen/problem" element={<RamenProblem />} />
                     <Route path="/paint/problem" element={<PaintProblem />} />
                 </Routes>
