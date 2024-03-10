e08a3b7e89ff32765902ba8c0d22babd080289b2
sooahn07 <23-022@ksa.hs.kr>
2024-03-10 15:21:59 +0900

    change test prob

--- src/3_Fire/data/problem_1.ts
+++ src/3_Fire/data/problem_1.ts
@@ -3,11 +3,10 @@ const F = "fire"
 const W = "wall"
 
 const map = [
-    [_, W, _, _, _],
-    [F, W, F, _, _],
-    [_, _, _, _, W],
-    [_, _, W, _, F],
-    [_, F, W, _, _]
+    [_, W, _, F],
+    [_, W, _, W],
+    [F, _, F, _],
+    [_, _, W, F]
 ]
 
 export { map }
