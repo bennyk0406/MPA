af585979c533a6bfb8fbe3f75f797d62cc3369e7
sooahn07 <23-022@ksa.hs.kr>
2024-03-19 08:30:19 +0900

    change problem num

--- src/3_Fire/data/problem_4.ts
+++ src/3_Fire/data/problem_4.ts
@@ -3,13 +3,16 @@ const F = "fire"
 const W = "wall"
 
 const map = [
-    [_, F, W, _, F, F, F],
-    [_, F, W, W, F, W, W],
-    [F, F, _, W, F, F, F],
-    [W, F, F, F, F, _, _],
-    [F, W, _, F, F, F, F],
-    [_, W, F, _, _, F, F],
-    [F, F, F, _, _, W, F]
+    [W, F, _, _, F, F, F, F, W, F],
+    [F, F, F, _, W, W, W, F, _, F],
+    [F, _, W, W, W, F, _, _, F, F],
+    [F, W, F, F, _, F, _, _, F, F],
+    [_, _, _, F, _, F, _, _, F, _],
+    [_, _, _, F, F, F, W, F, W, W],
+    [F, F, F, F, _, F, F, _, _, F],
+    [F, W, W, F, W, W, _, F, F, _],
+    [F, F, F, F, F, F, _, W, F, _],
+    [F, F, _, _, F, F, F, W, F, F]
 ]
 
 export { map }
